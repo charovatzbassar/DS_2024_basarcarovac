c3bd869b9c29b348611c751139242fce76685b76
charovatzbassar <carovac.basar@gmail.com>
2024-03-07 18:20:25 +0100

    fix

--- src/main/java/week2/TwoStackAlgorithm.java
+++ src/main/java/week2/TwoStackAlgorithm.java
@@ -6,18 +6,24 @@ public class TwoStackAlgorithm {
         Stack<Double> valueStack = new Stack<>();
         Stack<String> operatorStack = new Stack<>();
 
-        for (String e : expression.split("")) {
-            if (e.equals(" ")) continue;
+        for (String e : expression.split(" ")) {
             if (Utils.isNumeric(e)) {
                 valueStack.push(Double.parseDouble(e));
             } else if (Utils.isOperator(e)) {
                 operatorStack.push(e);
             } else if (e.equals(")")) {
                 String operator = operatorStack.pop();
-                Double v1 = valueStack.pop();
-                Double v2 = valueStack.pop();
 
-                valueStack.push(Utils.performOperation(v1, v2, operator));
+                Double v1 = valueStack.pop();
+                if (!operator.equals("√")) {
+                    Double v2 = valueStack.pop();
+
+                    valueStack.push(Utils.performOperation(v1, v2, operator));
+                } else {
+                    valueStack.push(Utils.performOperation(v1, 0, operator));
+                }
+
+
             }
 
         }
