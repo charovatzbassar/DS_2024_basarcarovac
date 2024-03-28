35b97eab9d5cdbbe4bfba87e78fc96cdab5de421
charovatzbassar <carovac.basar@gmail.com>
2024-03-29 00:12:58 +0100

    task

--- src/main/java/week5/Main.java
+++ src/main/java/week5/Main.java
@@ -2,12 +2,5 @@ package week5;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {5, 1, 9, -2, 8, 7, 12, 6, 11};
-
-        Sort.mergeSort(arr);
-
-        for (int i : arr) {
-            System.out.println(i);
-        }
     }
 }
