cd340cc8150ae364af74d801349b3af475e9ed59
charovatzbassar <carovac.basar@gmail.com>
2024-05-10 11:44:35 +0200

    5 lines

--- src/main/java/week10/HeapSort.java
+++ src/main/java/week10/HeapSort.java
@@ -1,17 +1,18 @@
 package week10;
 
+import java.util.Arrays;
+
 public class HeapSort {
 
     /* Heap sort algorithm for generic-type data */
     public static <Data extends Comparable<Data>> void sort(Data[] elements) {
         // your code here
         BinaryHeap<Data> heap = new BinaryHeap<>();
+        Arrays.stream(elements).forEach(heap::insert);
 
-        for (Data element : elements) {
-            heap.insert(element);
-        }
         for (int i = elements.length - 1; i >= 0; i--) {
             elements[i] = heap.deleteMax();
         }
+
     }
 }
