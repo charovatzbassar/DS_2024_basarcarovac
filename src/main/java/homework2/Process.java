b70d36e433857748a54410f93a64775a14b0aa8e
charovatzbassar <carovac.basar@gmail.com>
2024-05-22 11:30:35 +0200

    process class

--- src/main/java/homework2/Process.java
+++ src/main/java/homework2/Process.java
@@ -1,11 +1,52 @@
 package homework2;
 
 public class Process implements Comparable<Process> {
-    // Implement the required attributes and constructor
+    private String processName;
+    private int priority;
+    private double burstTime;
+    private double arriveTime;
+
+    public Process(String processName, int priority, double burstTime, double arriveTime) {
+        this.processName = processName;
+        this.priority = priority;
+        this.burstTime = burstTime;
+        this.arriveTime = arriveTime;
+    }
+
+    public String getProcessName() {
+        return processName;
+    }
+
+    public void setProcessName(String processName) {
+        this.processName = processName;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public double getBurstTime() {
+        return burstTime;
+    }
+
+    public void setBurstTime(double burstTime) {
+        this.burstTime = burstTime;
+    }
+
+    public double getArriveTime() {
+        return arriveTime;
+    }
+
+    public void setArriveTime(double arriveTime) {
+        this.arriveTime = arriveTime;
+    }
 
     @Override
     public int compareTo(Process other) {
-        // implement the correct logic
-        return -1;
+        return Integer.compare(this.getPriority(), other.getPriority());
     }
 }
