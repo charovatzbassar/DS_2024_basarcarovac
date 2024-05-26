e3ed9b8c5c2748e11559a24ed48115f332b6e762
charovatzbassar <carovac.basar@gmail.com>
2024-05-26 18:39:07 +0200

    done

--- src/main/java/homework2/Scheduler.java
+++ src/main/java/homework2/Scheduler.java
@@ -9,9 +9,12 @@ public class Scheduler {
     public static void scheduleAndRun(ArrayList<Process> processes) {
         int t = 0;
         int executedProcesses = 0;
+        int finishTimeSum = 0;
         ProcessQueue pq = new ProcessQueue();
         Process currentProcess = null;
         Process pausedProcess = null;
+        int burstTimeSum = processes.stream().mapToInt(Process::getBurstTime).sum();
+        int arriveTimeSum = processes.stream().mapToInt(Process::getArriveTime).sum();
 
         do {
             int finalT = t;
@@ -36,6 +39,7 @@ public class Scheduler {
 
             if (currentProcess != null && currentProcess.getBurstTime() == 0) {
                 executedProcesses++;
+                finishTimeSum += finalT + 1;
                 currentProcess = null;
 
                 if (pausedProcess != null) {
@@ -47,7 +51,9 @@ public class Scheduler {
             t++;
         } while (executedProcesses != processes.size());
 
+
         System.out.println("Total time: " + t);
+        System.out.println("Average waiting time: " + ((double)(finishTimeSum - burstTimeSum - arriveTimeSum) / processes.size()));
     }
 
     public static void main(String[] args) {
@@ -57,7 +63,7 @@ public class Scheduler {
         processes.add(new Process("P2", 2, 3, 0));
         processes.add(new Process("P3", 1, 7, 6));
         processes.add(new Process("P4", 3, 4, 11));
-        processes.add(new Process("P5", 2, 2, 24));
+        processes.add(new Process("P5", 2, 2, 12));
 
         scheduleAndRun(processes);
     }
