f0984cd98c8a37a857ea42558967d6eb5aad696e
charovatzbassar <carovac.basar@gmail.com>
2024-06-05 17:49:02 +0200

    friendships

--- src/main/java/homework4/Friendship.java
+++ src/main/java/homework4/Friendship.java
@@ -1,5 +1,36 @@
 package homework4;
 
 public class Friendship {
-    // implement the relevant properties, constructor and methods
+    private String firstFriend, secondFriend;
+    private int friendshipStrength;
+
+    public Friendship(String firstFriend, String secondFriend, int friendshipStrength) {
+        this.firstFriend = firstFriend;
+        this.secondFriend = secondFriend;
+        this.friendshipStrength = friendshipStrength;
+    }
+
+    public String getFirstFriend() {
+        return firstFriend;
+    }
+
+    public void setFirstFriend(String firstFriend) {
+        this.firstFriend = firstFriend;
+    }
+
+    public String getSecondFriend() {
+        return secondFriend;
+    }
+
+    public void setSecondFriend(String secondFriend) {
+        this.secondFriend = secondFriend;
+    }
+
+    public int getFriendshipStrength() {
+        return friendshipStrength;
+    }
+
+    public void setFriendshipStrength(int friendshipStrength) {
+        this.friendshipStrength = friendshipStrength;
+    }
 }
--- src/main/java/homework4/FriendshipRecommendation.java
+++ src/main/java/homework4/FriendshipRecommendation.java
@@ -1,10 +1,32 @@
 package homework4;
 
 public class FriendshipRecommendation implements Comparable<FriendshipRecommendation> {
-    // implement the relevant properties, constructor and methods
+    private String user;
+    private int recommendationStrength;
+
+    public FriendshipRecommendation(String user, int recommendationStrength) {
+        this.user = user;
+        this.recommendationStrength = recommendationStrength;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public int getRecommendationStrength() {
+        return recommendationStrength;
+    }
+
+    public void setRecommendationStrength(int recommendationStrength) {
+        this.recommendationStrength = recommendationStrength;
+    }
 
     @Override
     public int compareTo(FriendshipRecommendation o) {
-        return 0;
+        return Integer.compare(this.recommendationStrength, o.getRecommendationStrength());
     }
 }
