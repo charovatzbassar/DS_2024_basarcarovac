256c9043156baa04280f16483beade9bf57f252b
charovatzbassar <carovac.basar@gmail.com>
2024-06-05 18:09:44 +0200

    graph

--- src/main/java/homework4/Main.java
+++ src/main/java/homework4/Main.java
@@ -1,10 +1,12 @@
 package homework4;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws URISyntaxException, FileNotFoundException {
-        // main application logic
+        SocialNetwork sn = new SocialNetwork(new Scanner(new File("social_network.csv")));
     }
 }
--- src/main/java/homework4/SocialNetwork.java
+++ src/main/java/homework4/SocialNetwork.java
@@ -11,23 +11,48 @@ public class SocialNetwork {
     private HashMap<String, ArrayList<Friendship>> adj;
 
     public SocialNetwork() {
-        // implement the actual logic
+        this.V = 0;
+        this.E = 0;
+        this.adj = new HashMap<>();
     }
 
     public SocialNetwork(Scanner in) {
-        // implement the actual logic
+        this();
+        while (in.hasNextLine()) {
+            if (in.nextLine().contains("friendship_strength")) continue;
+            String[] line = in.nextLine().split(";");
+            Friendship friendship = new Friendship(line[0], line[1], Integer.parseInt(line[2]));
+            addUser(line[0]);
+            addUser(line[1]);
+            addFriendship(friendship);
+        }
     }
 
     public void addUser(String user) {
-        // implement the actual logic
+        if (adj.containsKey(user)) return;
+
+        adj.put(user, new ArrayList<>());
+        this.V++;
     }
 
     public void addFriendship(Friendship f) {
-        // implement the actual logic
+        if (!adj.containsKey(f.getFirstFriend()) || !adj.containsKey(f.getSecondFriend())) return;
+
+        adj.get(f.getFirstFriend()).add(f);
+        adj.get(f.getSecondFriend()).add(f);
+        this.E += 2;
     }
 
     public ArrayList<FriendshipRecommendation> recommendFriends(String user) {
         // implement the actual logic (remove next line)
         return null;
+    }
+
+    public int getV() {
+        return V;
+    }
+
+    public int getE() {
+        return E;
     }
 }
